627b0a949fe2c63732db656da5eadf1d3f80ba86
Mayank Agrawal <MayankAgrawal2005>
2024-12-21 11:02:58 +0530

    backend

---
+++ client/src/App.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import {Routes,Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
+import Profile from "./pages/Profile";
+import About from "./pages/About";
+import Header from "./components/Header";
+export default function App(){
+  return(
+
+      <div>
+        
+         <Header/>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/about" element={<About/>}/>
+          <Route path="/profile" element={<Profile/>}/>
+          <Route path="/sign-in" element={<Signin/>}/>
+          <Route path="/sign-up" element={<Signup/>}/>
+          
+        </Routes>
+
+      </div>
+   
+  )
+
+}
