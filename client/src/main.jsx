977d1b61c73a7111d1421523ae0d24f886da4b0a
Mayank Agrawal <MayankAgrawal2005>
2024-12-25 13:14:00 +0530

    add redux persist

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,13 +4,17 @@ import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import {store} from './redux/store.js';
+import {persistor} from './redux/store.js';
 import {Provider} from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
 createRoot(document.getElementById('root')).render(
    <Provider store={store}>
+   <PersistGate loading={null} persistor={persistor}>
   <BrowserRouter>
          <App />
     </BrowserRouter>
+    </PersistGate>
    </Provider>
    
    
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,10 +1,31 @@
-import {configureStore} from '@reduxjs/toolkit';
+import {combineReducers, configureStore} from '@reduxjs/toolkit';
 import userReducer from './user/userSlice';
+import { persistReducer ,persistStore} from 'redux-persist';
+import { version } from 'mongoose';
+import storage from 'redux-persist/lib/storage';
+
+
+const rootReducer = combineReducers({user:userReducer});
+
+const persistConfig = {
+    key:'root',
+    storage,
+    version:1,
+};
+
+const persistedReducer = persistReducer(persistConfig,rootReducer);
+
+
+
 export const store = configureStore({
-    reducer:{  user:userReducer },
+    // reducer:{  user:userReducer },
+    reducer: persistedReducer,
     // this is to prevent from the any error in the browser
     middleware:(getDefaultMiddleWare)=>getDefaultMiddleWare({
         serializableCheck:false,
     }),
 
 })
+
+
+export const persistor = persistStore(store);
