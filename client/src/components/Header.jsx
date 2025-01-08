d50851ea7d70ae8d245a954a5e0db61bb2dafff3
Mayank Agrawal <MayankAgrawal2005>
2025-01-08 14:42:45 +0530

    complete update header search form

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,10 +1,37 @@
-import React from 'react'
+import React, { useState,useEffect } from 'react'
 import { FaSearch } from "react-icons/fa";
 import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
 export default function Header() {
   const {currentUser} = useSelector(state=>state.user);
+  const [searchTerm,setSearchTerm]=useState('');
+  const navigate = useNavigate();
+
+   const handleSubmit = (e)=>{
+    e.preventDefault();
+     const urlParams = new URLSearchParams(window.location.search);
+
+     urlParams.set('searchTerm',searchTerm);
+
+     const searchQuery = urlParams.toString();
+
+     navigate(`/search?${searchQuery}`)
+     
+   }
+   useEffect(()=>{
+
+     const urlParams = new URLSearchParams(window.location.search);
+     const searchTermFromUrl = urlParams.get('searchTerm');
+
+     if(searchTermFromUrl){
+      setSearchTerm(searchTermFromUrl);
+     }
+
+
+   },[location.search])
+
   return (
 
     <div className='bg-slate-200 shadow-md '>
@@ -16,10 +43,18 @@ export default function Header() {
         </h1>
         </Link>
 
-        <form className='bg-slate-100 p-3 rounded-lg flex items-center'>
+        <form onSubmit={handleSubmit} className='bg-slate-100 p-3 rounded-lg flex items-center'>
+
+
             <input type="text" placeholder=' Search ...' 
-            className='bg-transparent focus:outline-none w-24 sm:w-64'/>
-            <FaSearch className='text-slate-600' />
+            className='bg-transparent focus:outline-none
+             w-24 sm:w-64'
+             value={searchTerm}
+             onChange={(e)=>setSearchTerm(e.target.value)} />
+
+             <button> <FaSearch className='text-slate-600'/></button>
+
+           
 
         </form>
 
@@ -49,11 +84,7 @@ export default function Header() {
            }
            </Link> 
 
-            {/* <Link to='/sign-in'>
-            <li className=' text-slate-700 hover:underline'>
-            Sign in
-            </li>
-            </Link>   */}
+            
         </ul>
     </div>
         
